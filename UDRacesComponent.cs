b6a26df6f63721e1c61edf901fb0943b5ececeab
Benjamin Führmann <fuhrmannb@users.noreply.github.com>
2019-02-15 13:20:06 +0100

    Initial commit

---
+++ UDRacesComponent.cs
@@ -0,0 +1,50 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplit.UI.Components
+{
+    public class UDRacesComponent : LogicComponent
+    {
+        private readonly LiveSplitState _state;
+
+        public override string ComponentName => "Ultime Décathlon Races";
+
+        public UDRacesComponent(LiveSplitState state)
+        {
+            this._state = state;
+            this.ContextMenuControls = new Dictionary<string, Action>();
+            this.ContextMenuControls.Add("UD Races...", new Action(() =>
+                {
+                    MessageBox.Show("Coucou");
+                }));
+        }
+
+        public override void Dispose()
+        {
+           
+        }
+
+        public override XmlNode GetSettings(XmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Control GetSettingsControl(LayoutMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetSettings(XmlNode settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            
+        }
+    }
+}
